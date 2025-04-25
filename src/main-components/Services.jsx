f9cb7e698418042a0c0f11461f4537ae44b309c6
IshantAhuja0 <ishantahuja2309@gmail.com>
2025-04-25 19:50:16 +0530

    error solved

--- src/main-components/Services.jsx
+++ src/main-components/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ServicesRender from '../components/services section/servicesRender';
+import ServicesRender from '../components/services-section/ServicesRender';
 
 function Services() {
   const categories = [
